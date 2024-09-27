596c2696129d7f0317bd1d8f078abbd493f32808
Tomás Macri <tomas.macri@vasscompany.com>
2024-09-27 10:43:14 +0200

    Create ExpensAppColorTheme

---
+++ ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/ui/theme/ExpensAppColorTheme.kt
@@ -0,0 +1,12 @@
+package com.tomasmacri.expensapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+data class ExpensAppColorTheme(
+    val purpleExpensApp: Color,
+    val expenseItemExpensApp: Color,
+    val backgroundColorExpensApp: Color,
+    val textColorExpensApp: Color,
+    val addIconColorExpensApp: Color,
+    val colorArrowRoundExpensApp: Color
+)
