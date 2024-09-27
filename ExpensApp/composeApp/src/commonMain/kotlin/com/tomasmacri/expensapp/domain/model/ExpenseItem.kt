88ebb467f6377ea8f0c3592f6f8e43400b3ff0af
Tomás Macri <tomas.macri@vasscompany.com>
2024-09-27 10:57:50 +0200

    Update ExpenseItem.amount type

--- ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/model/ExpenseItem.kt
+++ ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/model/ExpenseItem.kt
@@ -3,6 +3,6 @@ package com.tomasmacri.expensapp.domain.model
 data class ExpenseItem(
     val name: String,
     val description: String,
-    val amount: String,
+    val amount: Double,
     val category: ExpenseCategory
 )
