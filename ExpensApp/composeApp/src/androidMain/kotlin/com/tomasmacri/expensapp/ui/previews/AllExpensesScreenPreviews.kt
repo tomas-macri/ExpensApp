2afcfce0b0e90a4c97fa0e6feb9b2a598130f4bc
Tomás Macri <tomas.macri@vasscompany.com>
2024-09-30 11:07:37 +0200

    Add previews of AllExpensesScreen

---
+++ ExpensApp/composeApp/src/androidMain/kotlin/com/tomasmacri/expensapp/ui/previews/AllExpensesScreenPreviews.kt
@@ -0,0 +1,45 @@
+package com.tomasmacri.expensapp.ui.previews
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tomasmacri.expensapp.domain.model.Expense
+import com.tomasmacri.expensapp.domain.model.ExpenseCategory
+import com.tomasmacri.expensapp.ui.AllExpensesHeader
+import com.tomasmacri.expensapp.ui.ExpenseItem
+import com.tomasmacri.expensapp.ui.ExpensesTotalHeader
+import com.tomasmacri.expensapp.ui.theme.getColorsTheme
+
+@Composable
+fun BasePreviewWithPadding(padding: Int = 10, content: @Composable () -> Unit){
+    Box(modifier = Modifier.padding(padding.dp)) {
+        content()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ExpensesTotalHeaderPreview() {
+    BasePreviewWithPadding {
+        ExpensesTotalHeader(colors = getColorsTheme(), totalAmount = 8888.88, currency = "USD")
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun AllExpensesHeaderPreview() {
+    BasePreviewWithPadding {
+        AllExpensesHeader(colors = getColorsTheme()) {}
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ExpenseItemPreview() {
+    BasePreviewWithPadding {
+        ExpenseItem(colors = getColorsTheme(), expenseItem = Expense(1, "Food", "Date with my girlfriend", 25.0, ExpenseCategory.FOOD)) {}
+    }
+}
