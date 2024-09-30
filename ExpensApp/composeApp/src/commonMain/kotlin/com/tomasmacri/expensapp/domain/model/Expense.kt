c529b79445a320b4979a5207f16f2affc459e81b
Tomás Macri <tomas.macri@vasscompany.com>
2024-09-30 11:05:58 +0200

    Create a temporary list of expenses

--- ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/model/Expense.kt
+++ ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/model/Expense.kt
@@ -7,3 +7,16 @@ data class Expense(
     val amount: Double,
     val category: ExpenseCategory
 )
+
+val expenses = listOf(
+    Expense(1, "Groceries", "Weekly buy", 58.3, ExpenseCategory.GROCERIES),
+    Expense(2, "Dinner", "Anniversary", 15.2, ExpenseCategory.FOOD),
+    Expense(3, "Football tickets", "Racing Club vs Independiente", 25.3, ExpenseCategory.OTHER),
+    Expense(4, "Plane tickets", "Plane tickets to Madrid", 1164.0, ExpenseCategory.TRIP),
+    Expense(5, "Plane tickets", "Plane tickets to Madrid", 1164.0, ExpenseCategory.TRIP),
+    Expense(6, "Plane tickets", "Plane tickets to Madrid", 1164.0, ExpenseCategory.TRIP),
+    Expense(7, "Plane tickets", "Plane tickets to Madrid", 1164.0, ExpenseCategory.TRIP),
+    Expense(8, "Plane tickets", "Plane tickets to Madrid", 1164.0, ExpenseCategory.TRIP),
+    Expense(9, "Plane tickets", "Plane tickets to Madrid", 1164.0, ExpenseCategory.TRIP),
+    Expense(10, "Plane tickets", "Plane tickets to Madrid", 1164.0, ExpenseCategory.TRIP),
+)
