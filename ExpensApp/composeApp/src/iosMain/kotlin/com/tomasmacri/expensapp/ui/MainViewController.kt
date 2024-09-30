b7f4f69f6c9b66f57097f60835510994e2fb0798
Tomás Macri <tomas.macri@vasscompany.com>
2024-09-30 11:07:21 +0200

    Update MainActivity and MainViewController to use AllExpensesScreen

--- ExpensApp/composeApp/src/iosMain/kotlin/com/tomasmacri/expensapp/ui/MainViewController.kt
+++ ExpensApp/composeApp/src/iosMain/kotlin/com/tomasmacri/expensapp/ui/MainViewController.kt
@@ -2,4 +2,4 @@ package com.tomasmacri.expensapp.ui
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { AllExpensesScreen() }
