4907bd9e9ad50b23235177516d04708406755a55
Tomás Macri <tomas.macri@vasscompany.com>
2024-10-17 11:14:41 +0200

    Add default values for Expense

--- ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/model/Expense.kt
+++ ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/model/Expense.kt
@@ -2,8 +2,8 @@ package com.tomasmacri.expensapp.domain.model
 
 data class Expense(
     val id: Int,
-    val name: String,
-    val description: String,
-    val amount: Double,
-    val category: ExpenseCategory
+    val name: String = "",
+    val description: String = "",
+    val amount: Double = 0.0,
+    val category: ExpenseCategory = ExpenseCategory.OTHER
 )
