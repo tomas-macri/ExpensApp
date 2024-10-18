b46c791e3103850447ce85a69d76b3355b0b3a59
Tomás Macri <tomas.macri@vasscompany.com>
2024-10-18 10:54:07 +0200

    Remove unused name in ExpenseCategory

--- ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/model/ExpenseCategory.kt
+++ ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/model/ExpenseCategory.kt
@@ -1,11 +1,11 @@
 package com.tomasmacri.expensapp.domain.model
 
-enum class ExpenseCategory(name: String) {
-    GROCERIES("groceries"),
-    FOOD("food"),
-    TRIP("trip"),
-    HOUSE("house"),
-    PARTY("party"),
-    CONCERTS("concerts"),
-    OTHER("other")
+enum class ExpenseCategory {
+    GROCERIES,
+    FOOD,
+    TRIP,
+    HOUSE,
+    PARTY,
+    CONCERTS,
+    OTHER
 }
