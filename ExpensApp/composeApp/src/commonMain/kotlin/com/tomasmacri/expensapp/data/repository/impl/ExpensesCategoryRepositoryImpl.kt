efc10a5e9dd4aeb1089681094fb3602483ee566b
Tomás Macri <tomasmacri@gmail.com>
2024-10-30 13:21:18 +0100

    Apply Operation class to ExpenseCategoryRepository functions

--- ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/data/repository/impl/ExpensesCategoryRepositoryImpl.kt
+++ ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/data/repository/impl/ExpensesCategoryRepositoryImpl.kt
@@ -3,11 +3,15 @@ package com.tomasmacri.expensapp.data.repository.impl
 import com.tomasmacri.expensapp.data.manager.ExpenseCategoriesManager
 import com.tomasmacri.expensapp.data.repository.ExpenseCategoryRepository
 import com.tomasmacri.expensapp.domain.model.ExpenseCategory
+import com.tomasmacri.expensapp.domain.model.base.Operation
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
 
 class ExpensesCategoryRepositoryImpl(private val expenseCategoryManager: ExpenseCategoriesManager): ExpenseCategoryRepository {
-    override fun getAllExpenseCategories(): Flow<List<ExpenseCategory>> {
-        return flowOf(expenseCategoryManager.getAllCategories())
+    override fun getAllExpenseCategories(): Flow<Operation<List<ExpenseCategory>>> {
+        return flow {
+            emit(Operation.Loading())
+            emit(Operation.Success(expenseCategoryManager.getAllCategories()))
+        }
     }
 }
