3a7462b307cee20b050f275ab396c8ebd0a2adf7
Tomás Macri <tomasmacri@gmail.com>
2024-10-30 13:46:25 +0100

    Create usecases for every functionality

---
+++ ExpensApp/composeApp/src/commonMain/kotlin/com/tomasmacri/expensapp/domain/usecases/expense/GetExpenseUseCase.kt
@@ -0,0 +1,18 @@
+package com.tomasmacri.expensapp.domain.usecases.expense
+
+import com.tomasmacri.expensapp.data.repository.ExpensesRepository
+import com.tomasmacri.expensapp.domain.model.Expense
+import com.tomasmacri.expensapp.domain.model.base.Operation
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class GetExpenseUseCase(private val repository: ExpensesRepository) {
+
+    operator fun invoke(id: Int): Flow<Operation<Expense>> {
+        if (id <= 0) {
+            return flowOf(Operation.Error("The expense does not exist!"))
+        }
+        return repository.getExpense(id)
+    }
+
+}
