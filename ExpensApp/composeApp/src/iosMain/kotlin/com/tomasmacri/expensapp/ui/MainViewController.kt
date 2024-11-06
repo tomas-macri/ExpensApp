248dfbbec5418e75ea58d7e1a738e006be7be1c7
tomas-macri <tomasmacri@gmail.com>
2024-11-06 11:26:06 +0100

    Attach Koin to iOS app

--- ExpensApp/composeApp/src/iosMain/kotlin/com/tomasmacri/expensapp/ui/MainViewController.kt
+++ ExpensApp/composeApp/src/iosMain/kotlin/com/tomasmacri/expensapp/ui/MainViewController.kt
@@ -1,6 +1,13 @@
 package com.tomasmacri.expensapp.ui
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.tomasmacri.expensapp.ui.allexpenses.AllExpensesScreen
+import com.tomasmacri.expensapp.di.appModule
+import org.koin.core.context.startKoin
 
 fun MainViewController() = ComposeUIViewController { MainApp() }
+
+fun initKoin() {
+    startKoin {
+        modules(appModule)
+    }.koin
+}
