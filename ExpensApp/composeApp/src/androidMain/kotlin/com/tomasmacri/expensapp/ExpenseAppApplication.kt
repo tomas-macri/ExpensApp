2ebae63c5bf2c07cee2dce84ad6c017598821741
tomas-macri <tomasmacri@gmail.com>
2024-11-06 11:25:40 +0100

    Attach Koin to Android app

---
+++ ExpensApp/composeApp/src/androidMain/kotlin/com/tomasmacri/expensapp/ExpenseAppApplication.kt
@@ -0,0 +1,19 @@
+package com.tomasmacri.expensapp
+
+import android.app.Application
+import com.tomasmacri.expensapp.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class ExpenseAppApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@ExpenseAppApplication)
+            androidLogger()
+            modules(appModule)
+        }
+    }
+}
